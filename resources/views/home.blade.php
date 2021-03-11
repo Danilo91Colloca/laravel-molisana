92e255f95d66b51679d5c2f16fe7719b65de21fc
Danilo <danilo.colloca91@gmail.com>
2021-03-11 17:36:11 +0100

    bozza html della HOME

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -137,57 +137,208 @@
     <body>
       <div id="app">
         <header>
-
+          <div class="logo">
+            <img src= {{ url ('/img/brand.png') }} alt="">
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <a href="">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Prodotti
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  News
+                </a>
+              </li>
+            </ul>
+          </nav>
         </header>
         <main>
-          <div class="section lunga">
-            <div class="pastaType">
-              <span>le lunghe</span>
-            </div>
-            @foreach ($array as $key=>$value)
-              @if($value['tipo'] == 'lunga')
-                <div class="box leLunghe-product">
-                  <div class="img-container">
-                    <img src={{ $value['src'] }} alt="">
+          <div id="HOME">
+            <div class="section lunga">
+              <div class="pastaType">
+                <span>le lunghe</span>
+              </div>
+              @foreach ($array as $key=>$value)
+                @if($value['tipo'] == 'lunga')
+                  <div class="box leLunghe-product">
+                    <div class="img-container">
+                      <img src={{ $value['src'] }} alt="">
+                    </div>
                   </div>
-                </div>
-              @endif
-            @endforeach
-          </div>
-
-          <div class="section corta">
-            <div class="pastaType">
-              <span>le corte</span>
+                @endif
+              @endforeach
             </div>
-            @foreach ($array as $key=>$value)
-              @if($value['tipo'] == 'corta')
-                <div class="box leLunghe-product">
-                  <div class="img-container">
-                    <img src={{ $value['src'] }} alt="">
+  
+            <div class="section corta">
+              <div class="pastaType">
+                <span>le corte</span>
+              </div>
+              @foreach ($array as $key=>$value)
+                @if($value['tipo'] == 'corta')
+                  <div class="box leLunghe-product">
+                    <div class="img-container">
+                      <img src={{ $value['src'] }} alt="">
+                    </div>
                   </div>
-                </div>
-              @endif
-            @endforeach
-          </div>
-
-          <div class="section cortissima">
-            <div class="pastaType">
-              <span>le cortissime</span>
+                @endif
+              @endforeach
             </div>
-            @foreach ($array as $key=>$value)
-              @if($value['tipo'] == 'cortissima')
-                <div class="box leLunghe-product">
-                  <div class="img-container">
-                    <img src={{ $value['src'] }} alt="">
+  
+            <div class="section cortissima">
+              <div class="pastaType">
+                <span>le cortissime</span>
+              </div>
+              @foreach ($array as $key=>$value)
+                @if($value['tipo'] == 'cortissima')
+                  <div class="box leLunghe-product">
+                    <div class="img-container">
+                      <img src={{ $value['src'] }} alt="">
+                    </div>
                   </div>
-                </div>
-              @endif
-            @endforeach
+                @endif
+              @endforeach
+            </div>
           </div>
         </main>
 
         <footer>
+          <div class="logo logoFooter">
+            <img src={{ url ('/img/brand.png') }} alt="">
+           
+          </div>
+          <div>
+            <ul class="footerList contacts">
+              <li>Regione sociale: La Molisana S.P.A.</li>
+              <li>Sede legale: Contrada Colle delle Api, 100/A</li>
+              <li>86100-Campobasso (CB)</li>
+              <li>Pec: lamolisana@pec.it</li>
+              <li>Tel: +39 0874 4981</li>
+              <li>Fax: +39 0874 629584</li>
+              <li>info@lamolisana.it (per segnalazioni degli utent)</li>
+              <li>commerciale@lamolisana.it</li>
+              <li>export@lamolisana.it</li>
+              <li>numero verde 800818081</li>
+              <li>telefono 3801292455</li>
+            </ul>
 
+          </div>
+          <div>
+            <ul class="footerList pastificio">
+              <li>
+                <a href="">
+                  Il Pastificio
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Grano decorticato a pietra
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Il Molise c'è
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Filiera Integrata
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  100 anni di pasta
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Sartoria della psta
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Spaghetto Quadrato
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Le Persone
+                </a>
+              </li>
+            </ul>
+          </div>
+          
+          <div>
+            <ul class="footerList collezione">
+              <li>
+                <a href="">
+                  Collezione da Chef
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Grandi Cucine
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Biologiche
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Quadrate
+                </a>
+              </li>
+            </ul>  
+          </div>
+
+          <div class="footerList prodotti">
+            <ul>
+              <li>
+                <a href="">
+                  Le Classiche
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Le Integrali
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Le Speciali
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Le Biologiche
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Le Gluten-Free
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Le Semole
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  Le Extra di Lusso
+                </a>
+              </li>
+            </ul>
+          </div>
         </footer>
       </div>
       
