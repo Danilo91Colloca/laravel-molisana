e93c03f3ec3f50b6d78d2a4268a8c69229647238
Danilo <danilo.colloca91@gmail.com>
2021-03-12 15:38:11 +0100

    bozza chevron per cambiare img

--- resources/views/pages/productDetails.blade.php
+++ resources/views/pages/productDetails.blade.php
@@ -1,15 +1,28 @@
 @extends('layoutMaster')
 @section('main')
   <main class="main-productDetails">
-      <div class="product-title">
-        <span>{{ $pastaArray[$idPasta]['titolo'] }}</span>
+    {{-- chevron section --}}
+    <a href="">
+      <div class="chevron-left">
+          <i class="fas fa-chevron-left"></i>
       </div>
-      <div class="images-box">
-          <img src={{ $pastaArray[$idPasta]['src-h'] }} alt="">
-          <img src={{ $pastaArray[$idPasta]['src-p'] }} alt="">
+    </a>
+    <a href="">
+      <div class="chevron-right">
+          <i class="fas fa-chevron-right"></i>
       </div>
-      <div class="product-description">
-        <p>{!! nl2br($pastaArray[$idPasta]['descrizione']) !!}</p>
-      </div>
+    </a>
+    
+    {{-- /chevron section --}}
+    <div class="product-title">
+      <span>{{ $pastaArray[$idPasta]['titolo'] }}</span>
+    </div>
+    <div class="images-box">
+        <img src={{ $pastaArray[$idPasta]['src-h'] }} alt="">
+        <img src={{ $pastaArray[$idPasta]['src-p'] }} alt="">
+    </div>
+    <div class="product-description">
+      <p>{!! nl2br($pastaArray[$idPasta]['descrizione']) !!}</p>
+    </div>
   </main>
 @endsection
