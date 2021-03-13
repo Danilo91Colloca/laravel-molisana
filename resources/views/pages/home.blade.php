e679db9cc2100bae1f6b832f1bbaa8c39d6da37d
Danilo <danilo.colloca91@gmail.com>
2021-03-13 01:14:12 +0100

    grafica homePage terminata

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,9 +2,30 @@
 @extends('layoutMaster')
 @section('main')
   <main class="main-home">
-    <div class="img-welcome">
-      <img src="" alt="">
+    <div class="wrapper-opacizer">
+      <div class="lateral-img leftImg">
+        <img src="/img/pesci.jpeg" alt="">        
+      </div>
+      <div class="lateral-img rightImg">
+        <img src="/img/scorpione.jpeg" alt="">
+      </div>
+
+      <div class="titleContainer">
+        <div class="title">
+          <h1>nel segno della tenacità</h1>
+        </div>
+        <div class="subTitle">
+          <span>capsule collection</span>
+        </div>
+      </div>
+      <div class="img-welcome">
+        <img src="/img/piatto-toro-1.png" alt="">
+        <img src="/img/piatto-cancro-1.png" alt="">
+        <img src="/img/piatto-leone-1.png" alt="">
+        <img src="/img/piatto-bilancia-2.png" alt="">
+      </div>
     </div>
+    
 
   </main>
 
